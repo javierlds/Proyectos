c155e90b0534332bc460e60dad3af63098e43b99
Javier Paez <javierlds@gmail.com>
2023-09-21 20:23:52 -0500

    avance proyecto java

---
+++ ProyectosJava/registroCliente/src/main/java/com/registroCliente/registroCliente/RegistroClienteApplication.java
@@ -0,0 +1,13 @@
+package com.registroCliente.registroCliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroClienteApplication.class, args);
+	}
+
+}
---
+++ ProyectosJava/registroCliente/src/test/java/com/registroCliente/registroCliente/RegistroClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.registroCliente.registroCliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
